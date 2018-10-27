b52907e92aecb8a6b11e820479663aa4923908b5
steren55 <steren55@gmail.com>
2018-10-27 15:45:08 +0800

    Add NumberPlus Project

---
+++ NumberPlus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumberPlus
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i=1;i<=10;i++) {
+//				Console.Write("{0}\t", i);
+				int sum=0;
+				for (int j=1;j<=i;j++) {
+					sum+=j;
+					Console.Write("{0} ", j);
+					if (j!=i)
+						Console.Write("+ ");
+				}
+				Console.Write("= {0}", sum);
+				Console.WriteLine("");
+			}
+		}
+	}
+}
