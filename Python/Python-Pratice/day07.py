6429da674052c5be94ff7a9477e95b9f03d41b40
steren55 <steren55@gmail.com>
2019-05-31 22:49:43 +0800

    Update DAY 7

---
+++ Python/Python-Pratice/day07.py
@@ -0,0 +1,207 @@
+'''
+DAY 7
+'''
+import sys
+
+def fun1():
+    list1 = [1, 3, 5, 7, 100]
+    print(list1)
+    list2 = ['hello'] * 5
+    print(list2)
+    # 计算列表长度(元素个数)
+    print(len(list1))
+    # 下标(索引)运算
+    print(list1[0])
+    print(list1[4])
+    # print(list1[5])  # IndexError: list index out of range
+    print(list1[-1])
+    print(list1[-3])
+    list1[2] = 300
+    print(list1)
+    # 添加元素
+    list1.append(200)
+    list1.insert(1, 400)
+    list1 += [1000, 2000]
+    print(list1)
+    print(len(list1))
+    # 删除元素
+    list1.remove(3)
+    if 1234 in list1:
+        list1.remove(1234)
+    del list1[0]
+    print(list1)
+    # 清空列表元素
+    list1.clear()
+    print(list1)
+
+def fun2():
+    list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+    list2 = sorted(list1)
+    # sorted函数返回列表排序后的拷贝不会修改传入的列表
+    # 函数的设计就应该像sorted函数一样尽可能不产生副作用
+    list3 = sorted(list1, reverse=True)
+    # 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+    list4 = sorted(list1, key=len)
+    print(list1)
+    print(list2)
+    print(list3)
+    print(list4)
+    # 给列表对象发出排序消息直接在列表对象上进行排序
+    list1.sort(reverse=True)
+    print(list1)
+
+def fun3():
+    fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+    fruits += ['pitaya', 'pear', 'mango']
+	# 循环遍历列表元素
+    for fruit in fruits:
+        print(fruit.title(), end=' ')
+    print()
+    # 列表切片
+    fruits2 = fruits[1:4]
+    print(fruits2)
+    # fruits3 = fruits  # 没有复制列表只创建了新的引用
+    # 可以通过完整切片操作来复制列表
+    fruits3 = fruits[:]
+    print(fruits3)
+    fruits4 = fruits[-3:-1]
+    print(fruits4)
+    # 可以通过反向切片操作来获得倒转后的列表的拷贝
+    fruits5 = fruits[::-1]
+    print(fruits5)
+
+def fun4():
+    f = [x for x in range(1, 10)]
+    print(f)
+    f = [x + y for x in 'ABCDE' for y in '1234567']
+    print(f)
+    # 用列表的生成表达式语法创建列表容器
+    # 用这种语法创建列表之后元素已经准备就绪所以需要耗费较多的内存空间
+    f = [x ** 2 for x in range(1, 10)]
+    print(sys.getsizeof(f))  # 查看对象占用内存的字节数
+    for i in range(len(f)):
+        print("f =", f[i], "size:", sys.getsizeof(f[i]))  # 查看对象占用内存的字节数
+    print(f)
+    # 请注意下面的代码创建的不是一个列表而是一个生成器对象
+    # 通过生成器可以获取到数据但它不占用额外的空间存储数据
+    # 每次需要数据的时候就通过内部的运算得到数据(需要花费额外的时间)
+    f = (x ** 2 for x in range(1, 10))
+    print(sys.getsizeof(f))  # 相比生成式生成器不占用存储数据的空间
+    print(f)
+    for val in f:
+        print(val, end=' ')
+    print()
+
+def fun5():
+
+    def fib(n):
+        (a, b) = (0, 1)
+        for _ in range(n):
+            a, b = b, a + b
+            yield a
+    
+    for val in fib(10):
+        print(val, end=' ')
+
+def fun6():
+    # 定义元组
+    t = ('王叉叉', 38, True, '七上八下')
+    print(t)
+    # 获取元组中的元素
+    print(t[0])
+    print(t[3])
+    # 遍历元组中的值
+    for member in t:
+        print(member)
+    # 重新给元组赋值
+    # t[0] = '王大锤'  # TypeError
+    # 变量t重新引用了新的元组原来的元组将被垃圾回收
+    t = ('哈哈哈', 20, True, '一二三四')
+    print(t)
+    # 将元组转换成列表
+    person = list(t)
+    print(person)
+    # 列表是可以修改它的元素的
+    person[0] = '王朝'
+    person[1] = 25
+    print(person)
+    # 将列表转换成元组
+    fruits_list = ['apple', 'banana', 'orange']
+    fruits_tuple = tuple(fruits_list)
+    print(fruits_tuple)
+
+def fun7():
+    set1 = {1, 2, 3, 3, 3, 2}
+    print(set1)
+    print('Length =', len(set1))
+    set2 = set(range(1, 10))
+    print(set2)
+    set1.add(4)
+    set1.add(5)
+    set2.update([11, 12])
+    print(set1)
+    print(set2)
+    set2.discard(5)
+    # remove的元素如果不存在会引发KeyError
+    if 4 in set2:
+        set2.remove(4)
+    print(set2)
+    # 遍历集合容器
+    for elem in set2:
+        print(elem ** 2, end=' ')
+    print()
+    # 将元组转换成集合
+    set3 = set((1, 2, 3, 3, 2, 1))
+    print(set3.pop())
+    print(set3)
+    # 集合的交集、并集、差集、对称差运算
+    print(set1 & set2)
+    # print(set1.intersection(set2))
+    print(set1 | set2)
+    # print(set1.union(set2))
+    print(set1 - set2)
+    # print(set1.difference(set2))
+    print(set1 ^ set2)
+    # print(set1.symmetric_difference(set2))
+    # 判断子集和超集
+    print()
+    print(set1)
+    print(set2)
+    print(set3)
+    print(set2 <= set1)
+    # print(set2.issubset(set1))
+    print(set3 <= set1)
+    # print(set3.issubset(set1))
+    print(set1 >= set2)
+    # print(set1.issuperset(set2))
+    print(set1 >= set3)
+    # print(set1.issuperset(set3))
+
+def fun8():
+    import os
+    import time
+
+    str = "What the fuck you talking about!"
+    print(len(str))
+    for _ in range(len(str)):
+        os.system('cls')
+        print(str)
+        time.sleep(0.2)
+        str = str[1:]
+
+def main():
+    # fun1()
+    # fun2()
+    # fun3()
+    # fun4()
+    # fun5()
+    # fun6()
+    # fun7()
+
+# Homework
+# 1.
+    fun8()
+
+
+if __name__ == "__main__":
+    main()
